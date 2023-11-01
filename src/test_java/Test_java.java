7305588ae4318838d08712ffc1eac626b4644f21
Phanthai1601 <phanhoangthai765@gmail.com>
2023-11-01 17:31:06 +0700

    test thoi hehe

--- src/test_java/Test_java.java
+++ src/test_java/Test_java.java
@@ -3,7 +3,10 @@ package test_java;
 public class Test_java {
 
     public static void main(String[] args) {
-        System.out.print("hello world");
+        System.out.println("hello world");
+        for(int i=0;i<16;i++){
+            System.out.print(i+" ");
+        }
     }
     
 }
