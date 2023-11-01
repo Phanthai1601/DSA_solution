dcb30b49d7e1f589e2064250a66534d704e46267
Phanthai1601 <phanhoangthai765@gmail.com>
2023-11-01 20:31:21 +0700

    xinchao

---
+++ src/test_java/java_xinchao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test_java;
+
+/**
+ *
+ * @author Admin
+ */
+public class java_xinchao {
+    public static void main(String [] args){
+        System.out.print("hello world");
+    }
+}
